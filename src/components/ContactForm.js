16e1f2688b56b6ef6d33fd4e7be2c8dedc5c29d4
Elba <elbaaak@gmail.com>
2023-09-15 15:56:54 +0200

    done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseUrl = 'https://progboard.app-med.com/api/client';
+const baseUrl = 'https://progboard.app-med.com/api/contact';
 const ContactForm = () => {
 
     const initialFormData = {
